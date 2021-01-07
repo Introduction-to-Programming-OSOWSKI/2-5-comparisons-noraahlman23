6f886c06df5d8a4d0d6aa0ffc2c9b08440af7cab
noraahlman23 <819446@mystma.org>
2021-01-07 16:35:22 +0000

    functions

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-#WRITE YOUR CODE IN THIS FILE
+def greaterThan(x,y):
+    if x > y:
+        return True
+    else: 
+        return False
+
+print (greaterThan(2,1))
+
+def lessThan(x,y):
+    if x < y:
+        return True
+    else:
+        return False
+print (lessThan(1,2))
+
+def equalTo(x,y):
+    if x == y:
+        return True
+    else:
+        return False
+
+print (equalTo(3,3))
+
+def greaterOrEqual(x,y):
+    if x >= y:
+        return True 
+    else:
+        return False
+
+print (greaterOrEqual(4,3))
+
+def lessOrEqual(x,y):
+    if x <= y:
+        return True
+    else:
+        return False 
+
+print (lessOrEqual(3,4))
